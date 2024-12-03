e8ac55af67349fd59ba0a525f9b9a82d9042246d
Igor Richter <i.rixter201572@gmail.com>
2024-12-03 19:32:38 +0300

    fix linter

--- src/pages/error-page/error-page.tsx
+++ src/pages/error-page/error-page.tsx
@@ -5,17 +5,17 @@ function ErrorPage(): JSX.Element {
   return (
     <>
       <Header/>
-        <div className='not-found'>
-          <div className="container">
-            <div className="not-found__wrapper">
-              <div className="not-found__number">
-                4<span>0</span>4
-              </div>
-              <div className="not-found__title">Page <span>Not Found</span></div>
-              <div className="not-found__subtitle">go to <a className='not-found__link' href='/'>back</a></div>
+      <section className='not-found'>
+        <div className="container">
+          <div className="not-found__wrapper">
+            <div className="not-found__number">
+              4<span>0</span>4
             </div>
+            <div className="not-found__title">Page <span>Not Found</span></div>
+            <div className="not-found__subtitle">go to <a className='not-found__link' href='/'>back</a></div>
           </div>
         </div>
+      </section>
     </>
   );
 }
